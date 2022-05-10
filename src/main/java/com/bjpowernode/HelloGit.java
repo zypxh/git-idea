469777c474649844265506f2626103def0c89931
zypxh <1394416483@qq.com>
2022-05-10 09:43:51 +0800

    second commit

--- src/main/java/com/bjpowernode/HelloGit.java
+++ src/main/java/com/bjpowernode/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("idea集成git");
         System.out.println("idea集成git");
+        System.out.println("idea集成git2");
     }
 }
